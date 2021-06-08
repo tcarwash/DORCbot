6e58ff438e108b7b33aaabe0fd755e71f39be2b4
Josh Mathis <joshmathis+github@gmail.com>
2021-06-08 05:15:43 -0700

    Add support for solar, refactor structure

--- dorcbot/dorcbot.py
+++ dorcbot/dorcbot.py
@@ -1,14 +1,17 @@
 import os
 import discord
+import re
+import requests
 from dotenv import load_dotenv
 from tabulate import tabulate
-import requests
+from xml.etree import ElementTree
 
 load_dotenv()
 
 TOKEN = os.environ.get('TOKEN')
 
 client = discord.Client()
+
 
 def get_spots():
     spots = requests.get('https://dorc-stats.ag7su.com/data/3').json()
@@ -21,23 +24,70 @@ def get_spots():
     spots = payload + tabulate(tab, header)
     return spots
 
+
+def get_solar():
+    payload = "Solar Indices:\n\n"
+    solarcontent = requests.get('https://joshmathis.com/dorc/solarxml.xml')
+    tree = ElementTree.fromstring(solarcontent.content).find("./solardata")
+    tab = []
+    # Goofy spacing in the header because I don't know how to send fixed-width messages yet.
+    header = ['As of               ', '     A  ', '     K  ', '     SFI ']
+    row = [tree.find("./updated").text,
+           tree.find("./aindex").text,
+           tree.find("./kindex").text,
+           tree.find("./solarflux").text]
+    tab.append(row)
+    solar = payload + tabulate(tab, header)
+    return solar
+
+
+def get_help():
+    # Dynamically create help based on the defined commands (commandmap)
+    payload = "Usage: "
+    for key in commandmap.keys():
+        command = "\n\t" + key + " -- " + commandmap.get(key)[1]
+        payload += command
+    return payload
+
+
+# Add commands here. Format:
+#    '!command': [function_to_create_final_output], "Help text"
+commandmap = {
+    '!spots': [get_spots, "Get the 3 most recent spots of DORC members"],
+    '!solar': [get_solar, "Get solar conditions"],
+    '!help': [get_help, "Get the thing you're reading now"]
+}
+
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+
 @client.event
 async def on_message(message):
-    if message.content.startswith('!spots'):
-        payload = get_spots()
+    if message.author == client.user:
+        return
+
+    isBang = False
+    if message.content.startswith('!'):
+        command = re.match(r"[^\s]+", message.content).string
+
+        func = commandmap.get(command)
+        if func is None:
+            payload = "Unsupported command. Try asking for !help instead."
+
+        else:
+            payload = func[0]()
+
         await message.channel.send(payload)
 
-    if message.content.startswith('!help'):
-        payload ="""
-        Usage:
-            !help --  Display this message 
-            !spots --  Get the 3 most recent spots of DORC members
+    else:
+        return
+        # non-command branch. @Mention Chat capabilities, maybe, eventually?
+        # To use this, we need to check if the bot is mentioned in the message using
+        #  if client.user.mentioned_in(message)
+        # payload = "Only ! commands are supported. Ask for !help instead."
 
-        """
-        await message.channel.send(payload)
 
 client.run(TOKEN)
